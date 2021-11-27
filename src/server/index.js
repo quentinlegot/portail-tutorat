8ee9a9ba1df9ecbf88a90ac90b00e32788adf0f2
Quentin Legot <legotquentin@gmail.com>
2021-11-27 19:07:56 +0100

    add basics express instructions

--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,16 @@
-console.log("main")
+const path = require('path')
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.set('view engine', 'ejs')
+app.set('views', path.resolve( __dirname, "views"))
+
+app.use('/static', express.static(path.resolve(__dirname, "static")))
+.get('/', (req, res) => {
+    res.status(200).render('index', {})
+})
+
+app.listen(port, () => {
+    console.log('Server listening to port ' + port + " and dir " + __dirname)
+})
